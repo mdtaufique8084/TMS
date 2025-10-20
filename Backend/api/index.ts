a38720da2d8a3bcbd837a239f446bd72bba8fe69
Md Taufique <mdtaufique8084@gmail.com>
2025-10-20 12:04:04 +0530

    changes

--- Backend/api/index.ts
+++ Backend/api/index.ts
@@ -1,18 +1,4 @@
 import serverless from "serverless-http";
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import dotenv from "dotenv";
-
-import authRoutes from "../src/routes/authRoutes";
-import taskRoutes from "../src/routes/taskRoutes";
-
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
+import { app } from "../src/index";
 
 export default serverless(app);
--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,7 +5,8 @@ import authRoutes from "./routes/authRoutes";
 import taskRoutes from "./routes/taskRoutes";
 
 dotenv.config();
-const app = express();
+
+export const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -15,11 +16,9 @@ app.get("/", (_req, res) => res.json({ message: "Task Manager API running" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-export default app;
-
 if (process.env.NODE_ENV !== "test") {
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () =>
-    console.log(` Server running on http://localhost:${PORT}`)
+    console.log(`Server running on http://localhost:${PORT}`)
   );
 }
